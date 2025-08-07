39e2ce17dbdfafa0d33aceb9fa3c9bc33ebae9b7
Jar1c <148020531+Jar1c@users.noreply.github.com>
2025-08-07 09:49:59 +0000

    Activity 1 and 2 : Understanding the Dart

--- Act2.dart
+++ Act2.dart
@@ -1,4 +1,17 @@
-void main () {
-    int rectPerimiter = calculatePerimeter(10,5)
-    print(The perimiter of rectangle is)
+void main() {
+// Calling for a rectangle
+int rectPerimeter = calculatePerimeter(10, 5);
+print("The perimeter of the rectangle is: $rectPerimeter");
+
+// Calling for square
+int squarePerimeter = calculatePerimeter(7, 0, isSquare: true);
+print("The perimeter of the square is: $squarePerimeter");
+}
+
+int calculatePerimeter(int width, int height, {bool isSquare = false}) {
+if (isSquare) {
+return 4 * width;
+} else {
+return 2 * (width + height);
+}
 }
