6d3fe973a2a401fa07c162d02caf4f507610d06c
Jar1c <148020531+Jar1c@users.noreply.github.com>
2025-08-07 09:44:13 +0000

    Activity 1 and 2 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,16 @@
+void main () {
+  String greeting = "Welcome";
+  String user = "Alice";
+
+print("$greeting, ${user}!");
+}
+
+
+void main () {
+  String greeting = 'Welcome';
+  dynamic user = 25;
+  
+print("$greeting, ${user}");
+}
+
+// the variable now user change to dynamic so now can use string and int
---
+++ Act2.dart
@@ -0,0 +1,4 @@
+void main () {
+    int rectPerimiter = calculatePerimeter(10,5)
+    print(The perimiter of rectangle is)
+}
